308df87922eac79ef5c1021dea6162361455115d
gastonperego <gastonperego@gmail.com>
2024-02-08 17:25:15 -0300

    feat: schema user

--- schemas/User.js
+++ schemas/User.js
@@ -1,24 +1,28 @@
 const { z } = require('zod')
+const phoneRegex = new RegExp(
+  /^([+]\d{1,3})(\d*)$/
+);
 
 const userSchema = z.object({
-  name: z.string().max(50),
-  surname: z.string().max(50),
-  ci: z.string().min(8).max(10),
-  email: z.string().email({
-    message: "Write a correct email"
-  }),
-  password: z.string().min(6, "Password too short").max(30),
-  phone_number: z.string().max(50),
-  emergency_number: z.string().max(50),
-  insurance_number: z.string().max(50),
+  name: z.string().min(1, {message: "Name cannot be empty"}).refine((value) => {return /^\D+$/.test(value);}, {message: 'Name must not contain numeric values'}),
+  surname: z.string().refine((value) => {return /^\D+$/.test(value);}, {message: 'Surname must not contain numeric values'}),
+  password: z.string('Invalid password').min(8).max(30),
+  CI: z.string().min(1),
+  email: z.string().min(1, { message: "This field has to be filled." }).email("This is not a valid email."),
+  birth_date: z.coerce.date(),
+  phone_number: z.string().min(10).regex(phoneRegex, 'Must contain the phone prefix eg. +598...'),
+  emergency_number: z.string().min(10).regex(phoneRegex, 'Must contain the phone prefix eg. +598...'),
+  insurance: z.string().min(1)
 });
 
+
 const userUpdateSchema = z.object({
-    email: z.string().email('Invalid email').max(30),
-    password: z.string('Invalid password').min(8).max(30),
-    phone_number: z.string('Invalid number').max(50),
-    emergency_number: z.string('Invalid number').max(50),
-    insurance_number: z.string('Invalid number').max(50),
+  email: z.string().min(1, { message: "This field has to be filled." }).email("This is not a valid email."),
+  password: z.string('Invalid password').min(8).max(30),
+  phone_number: z.string().min(10).regex(phoneRegex, 'Must contain the phone prefix eg. +598...'),
+  emergency_number: z.string().min(10).regex(phoneRegex, 'Must contain the phone prefix eg. +598...'),
+  insurance: z.string().min(1),
+  role_id: z.number().max(6).array().nonempty()
 })
 
 module.exports = {
